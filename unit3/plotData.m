6464c9954bc5abe3e089909d491546a8f6dbcad0
cc1987916 <zylald@126.com>
2022-01-27 19:57:11 +0800

    Update plotData.m

--- unit3/plotData.m
+++ unit3/plotData.m
@@ -11,8 +11,15 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
-
-
+%data = load ('ex2data1.txt');
+%X = data(:,1:2);
+%y = data(:,end);
+pos = find(y==1);
+neg = find(y==0);
+plot(X(pos, 1), X(pos, 2), 'r+', 'lineWidth', 2, 'MarkerSize', 5);
+plot(X(neg, 1), X(neg, 2), 'ko', 'lineWidth', 2, 'MarkerSize', 5);
+%scatter(X(pos, 1), X(pos, 2), 9, 'r', 'filled')
+%scatter(X(neg, 1), X(neg, 2), 9, 'k', '+')
 
 
 
