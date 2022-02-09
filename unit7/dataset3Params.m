d4f60bc2370c6e844570fa4b71753ca2ba6a76a0
cc1987916 <zylald@126.com>
2022-02-09 13:57:25 +0800

    updating

--- unit7/dataset3Params.m
+++ unit7/dataset3Params.m
@@ -22,7 +22,25 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+vec_const = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+C_optimal = inf;
+sigma_optimal = inf;
+err_min = inf;
 
+for i = 1:length(vec_const),
+    C_curr = vec_const(i);
+    for j = 1:length(vec_const),
+      sigma_curr = vec_const(j);
+      model = svmTrain(X, y, C_curr, @(x1, x2) gaussianKernel(x1, x2, sigma_curr));
+      predictions = svmPredict(model, Xval);
+      pre_err = mean(double(predictions ~= yval));
+      if pre_err < err_min
+        C_optimal = C_curr;
+        sigma_optimal = sigma_curr;
+        err_min = pre_err;
+      endif;
+    end;
+end;
 
 
 
